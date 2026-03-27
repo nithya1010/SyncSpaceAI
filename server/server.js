c452a5194c7c9f41d10f476d3c80df5a137ea174
vaishnavee07 <vaishnaveem.csbs2024@citchennai.net>
2026-03-27 21:45:58 +0530

    fix: allow CORS for both localhost:5173 and 5174

--- server/server.js
+++ server/server.js
@@ -10,7 +10,10 @@ connectDB()
 
 const app = express()
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  credentials: true
+}))
 app.use(express.json())
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
